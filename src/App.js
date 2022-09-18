18333d1b7b2c96bd7f3b1d6deb27bb7fa8465b20
Ryu, Taeseok <tryu2@illinois.edu>
2022-09-18 14:30:48 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Test from "./components/Test"
+import {useState} from "react"
 
 function App() {
+
+  const [hide,setHide] = useState(false);
+
   return (
     <div className="App">
-     <Test />
-     
+     {hide && <Test />}
+     <button  onClick={()=>setHide(!hide)} >CLICK</button>
     </div>
   );
 }
